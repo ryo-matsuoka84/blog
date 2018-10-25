c49e70029cd5cc179de73ba9320162a137aea70a
ryo-matsuoka84 <matsuoka-ryo@metawater.co.jp>
2018-10-25 18:15:29 +0900

    requierとpermit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
   def new
   end
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    
+    @article.save
+    redirect_to @article
   end
 end
