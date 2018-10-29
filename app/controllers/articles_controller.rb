5f2fc50f36441071aaa2850193e0307d8c6dcdd8
ryo-matsuoka84 <matsuoka-ryo@metawater.co.jp>
2018-10-29 09:42:53 +0900

    記事を更新する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,20 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+  @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @articles = Article.all
   end
